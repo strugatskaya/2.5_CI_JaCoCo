a89c2e5c14bde87e2b3f7061ea1f35edbd666281
Juliya Strugatskaya <strugatskaya.um@gmail.com>
2021-06-13 17:19:32 +0300

    100% coverage

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatisticsServiceTest {
 
@@ -18,16 +18,16 @@ class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
-    @Test
+    {
 
-    void findCurrent_Max(){
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
 
-        long actual = service.findCurrent_Max(incomesInBillions);
+        long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
+
     }
 }
