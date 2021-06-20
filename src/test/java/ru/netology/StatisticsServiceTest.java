dc7c81fc3cdbf636436a6eac9474972727e1a813
Juliya Strugatskaya <strugatskaya.um@gmail.com>
2021-06-20 19:43:58 +0300

    test changed

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -18,16 +18,16 @@ class StatisticsServiceTest {
         assertEquals(expected, actual);
     }
 
-    {
 
+    @Test
+    void FindCurrentMaxIncomeEquals() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {11, 8, 5, 4, 3,5, 6, 8, 11, 12, 12};
         long expected = 12;
 
         long actual = service.findMax(incomesInBillions);
 
         assertEquals(expected, actual);
-
     }
 }
